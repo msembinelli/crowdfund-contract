bb8a2c04369fcd18d241e4a7c20baf390b420853
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-11 15:43:11 -0700

    Add Crowdfund contract

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -1,5 +1,67 @@
 pragma solidity ^0.4.17;
 
 contract Crowdfund {
+    struct Request {
+        string description;                  // Description of spending request
+        uint value;                          // Amount of ether (wei)
+        address recipient;                   // Payee/person/organization receiving funds
+        bool complete;                       // Has the money been sent
+        uint approvalCount;                  // Count the number of approvals
+        mapping(address => bool) approvals;  // Track who has voted
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
 
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Crowdfund(uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value >= minimumContribution);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+    
+        request.approvalCount++;
+        request.approvals[msg.sender] = true;
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+        
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
 }
