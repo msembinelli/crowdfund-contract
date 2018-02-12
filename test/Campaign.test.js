0126cd646ca0bf8eab7cd1a55ec38f1381a29d12
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-11 18:59:36 -0700

    Add test to ensure contributors are approvers

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -47,4 +47,14 @@ describe('Campaigns', () => {
         const manager = await campaign.methods.manager().call();
         assert.equal(accounts[0], manager)
     });
+
+    it('allows people to contribute and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+
+        const isApprover = await campaign.methods.approvers(accounts[1]).call();
+        assert(isApprover);
+    });
 });
