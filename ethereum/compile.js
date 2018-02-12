5e4e008dd7a605d5ebfd353d3037deee4e37742e
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-11 18:31:07 -0700

    Make compile.js generic, output to build folder

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,8 +1,28 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
+const yaml = require('yamljs');
 
-const contractPath= path.resolve(__dirname, 'contracts', 'Campaign.sol')
-const source = fs.readFileSync(contractPath, 'utf8');
+config = yaml.load('config.yml');
 
-module.exports = solc.compile(source, 1).contracts[':Campaign'];
+const buildPath = path.resolve(__dirname, config.contracts.output);
+fs.removeSync(buildPath);
+
+let contractPath;
+let source;
+let output;
+
+for (let fileIndex in config.contracts.files) {
+    contractPath = path.resolve(__dirname, config.contracts.input, config.contracts.files[fileIndex]);
+    source = fs.readFileSync(contractPath, 'utf8');
+    output = solc.compile(source, 1).contracts;
+
+    fs.ensureDirSync(buildPath);
+
+    for (let contract in output) {
+        fs.outputJsonSync(
+            path.resolve(buildPath, contract.replace(':', '') + '.json'),
+            output[contract]
+        );
+    }
+}
