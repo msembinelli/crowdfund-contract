affc101b2b37836eef82afe0578a81f29dc4b45f
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-13 00:16:39 -0700

    Add error output for creating campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
 
     };
 
@@ -26,7 +31,7 @@ class CampaignNew extends Component {
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -37,7 +42,14 @@ class CampaignNew extends Component {
                         />
                     </Form.Field>
 
+                    <Message
+                        error
+                        header="Error!"
+                        content={this.state.errorMessage}
+                    />
+
                     <Button primary>Create!</Button>
+
                 </Form>
             </Layout>
         );
