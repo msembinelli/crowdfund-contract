aaa768fb1e7343851acb36ae5fc4fd1e39a182f4
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-12 23:53:53 -0700

    Update component state for minimumContribution

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Button, Form, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    };
+
     render() {
         return (
             <Layout>
-                <h1>Campaign New!</h1>
+                <h3>Create a Campaign</h3>
+
+                <Form>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <Input
+                            label="wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={ event => this.setState({ minimumContribution: event.target.value })}
+                        />
+                    </Form.Field>
+
+                    <Button primary>Create!</Button>
+                </Form>
             </Layout>
         );
     }
