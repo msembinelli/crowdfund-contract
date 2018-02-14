b9d9d833f3703cba5a150485ad0f8a6cc2b817af
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-13 21:26:48 -0700

    Add non-functioning form to campaign show view

---
+++ components/ContributeForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+
+class ContributeForm extends Component {
+    render() {
+        return (
+            <Form>
+                <Form.Field>
+                    <label>Amount to Contribute</label>
+                    <Input
+                        label="ether"
+                        labelPosition="right"
+                    />
+                </Form.Field>
+                <Button primary>
+                    Contribute!
+                </Button>
+            </Form>
+        );
+    };
+};
+
+export default ContributeForm;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
    static async getInitialProps(props) {
@@ -65,7 +66,15 @@ class CampaignShow extends Component {
     render() {
         return (
             <Layout>
-                {this.renderCards()}
+                <Grid>
+                    <Grid.Column width={10}>
+                        {this.renderCards()}
+                    </Grid.Column>
+
+                    <Grid.Column width={6}>
+                        <ContributeForm />
+                    </Grid.Column>
+                </Grid>
             </Layout>
         );
     }
