d5fbecd7455be1f91a2e1d54f7f2339c819ba81e
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-13 22:03:26 -0700

    Add requests routing and button

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Button, Card, Grid } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import { Card, Grid } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
@@ -68,13 +69,25 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <Grid>
-                    <Grid.Column width={10}>
-                        {this.renderCards()}
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCards()}
+                        </Grid.Column>
 
-                    <Grid.Column width={6}>
-                        <ContributeForm address={this.props.address} />
-                    </Grid.Column>
+                        <Grid.Column width={6}>
+                            <ContributeForm address={this.props.address} />
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={`/campaigns/${this.props.address}/requests`}>
+                                <a>
+                                    <Button primary>View Requests</Button>
+                                </a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Layout>
         );
